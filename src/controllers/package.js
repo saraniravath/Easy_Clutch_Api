2454d49eeea48e5684850d4ac6dd42767edb5dbf
Antony Cijo <antonythattarkunnel@gmail.com>
2023-06-03 16:41:14 +0530

    payment notification and verification api, schedule api added

--- src/controllers/package.js
+++ src/controllers/package.js
@@ -1,4 +1,9 @@
-import { activatePackage, getTraineeIdOfPackage, listPackagesModel, updatePackagesModel } from "../models/package.js"
+import {
+    activatePackage, addScheduleModel, checkVehicleActive, checkVehicleUsage, getAvailiableSession, getRemainingSession, getRequestpackage,
+    getScheduleForTraineeModel,
+    getScheduleForTrainerModel,
+    getTraineeIdOfPackage, insertPackageModel, listPackagesModel, updatePackagesModel
+} from "../models/package.js"
 
 export const listPackagesController = (name) => {
     return listPackagesModel(name)
@@ -8,12 +13,47 @@ export const updatePackagesController = (id, price) => {
     return updatePackagesModel(id, price)
 }
 
-export const verifyPackagePaymentController = async (id) => {
-    const traineeId = await getTraineeIdOfPackage(id)
-    if (traineeId) {
-        await activatePackage(traineeId)
+export const insertPackageController = async (id, packageDetails) => {
+    const availiable = await getAvailiableSession()
+    if (availiable > 0) {
+        console.log(packageDetails, availiable)
+        await insertPackageModel(id, packageDetails, availiable)
         return true;
     }
     return false;
+}
 
+export const displayRequestPackageController = async () => {
+    return await getRequestpackage()
+}
+
+export const verifyPackagePaymentController = async (id) => {
+    const traineeId = await getTraineeIdOfPackage(id)
+    if (traineeId) {
+        await activatePackage(id)
+        return true;
+    }
+    return false;
+}
+
+
+export const addScheduleController = async (id, scheduleDetails) => {
+    const packageDetails = await getRemainingSession(id)
+    const checkVehicle = await checkVehicleActive(scheduleDetails.vehicleNameId)
+    const checkUsage = await checkVehicleUsage(scheduleDetails)
+    if (packageDetails > 0 && checkVehicle && checkUsage) {
+        const response = await addScheduleModel(id, scheduleDetails)
+        if (response) {
+            return true
+        }
+    }
+    return 0
+}
+
+export const getScheduleController = (user) => {
+    if (user.userType === 'trainee') {
+        return getScheduleForTraineeModel()
+    } else {
+        return getScheduleForTrainerModel()
+    }
 }
--- src/models/package.js
+++ src/models/package.js
@@ -30,7 +30,7 @@ export const updatePackagesModel = async (id, price) => {
 
 
 export const activatePackage = async (id) => {
-    const result = await pool.query("UPDATE package SET active= 1 WHERE trainee_id = ?", [id]);
+    const result = await pool.query("UPDATE package SET active= 1 WHERE id = ?", [id]);
     if (result[0].affectedRows === 0) {
         return false
     }
@@ -43,4 +43,80 @@ export const getTraineeIdOfPackage = async (id) => {
         return rows[0];
     }
     return 0;
+}
+
+export const getRequestpackage = async () => {
+    const [rows, fields] = await pool.query("SELECT package.*,trainee.first_name,trainee.last_name,vehicle.type FROM package,trainee,vehicle WHERE package.active=0 AND trainee.id=package.trainee_id AND package.package_vehicle_type_id=vehicle.id");
+    if (rows) {
+        return rows;
+    }
+    return 0;
+}
+
+export const getAvailiableSession = async () => {
+    const [rows, fields] = await pool.query("SELECT session_per_package FROM trainer");
+    if (rows) {
+        return rows[0].session_per_package;
+    }
+    return 0;
+}
+
+export const insertPackageModel = async (id, packageDetails, availiable) => {
+    const result = await pool.query("INSERT INTO package(trainee_id,transaction_id,package_vehicle_type_id,remaining_sessions) values(?,?,?,?)",
+        [id, packageDetails.transactionId, packageDetails.vehicleType, availiable]);
+    if (result[0].affectedRows === 0) {
+        return false
+    }
+    return true
+}
+
+export const getRemainingSession = async (id) => {
+    const [rows, fields] = await pool.query("SELECT remaining_sessions FROM package WHERE id=? AND active=1", [id]);
+    if (rows[0]) {
+        return rows[0].remaining_sessions
+    }
+    return 0
+}
+
+export const checkVehicleActive = async (id) => {
+    const [rows, fields] = await pool.query("SELECT * FROM vehicle WHERE id=? AND active=1", [id]);
+    if (rows[0]) {
+        return true
+    }
+    return 0
+}
+
+export const checkVehicleUsage = async (scheduleDetails) => {
+    const [rows, fields] = await pool.query("SELECT * FROM booked_session WHERE vehicle_id=? AND date = ? AND FN_or_AN = ?",
+        [scheduleDetails.vehicleNameId, scheduleDetails.date, scheduleDetails.time]);
+    if (rows[0]) {
+        return 0
+    }
+    return true
+}
+
+export const addScheduleModel = async (id, scheduleDetails) => {
+    const insert = await pool.query("INSERT INTO booked_session(package_id, date, FN_or_AN, vehicle_id) values(?,?,?,?)",
+        [id, scheduleDetails.date, scheduleDetails.time, scheduleDetails.vehicleNameId]);
+    await pool.query("UPDATE package SET remaining_sessions=remaining_sessions-1 WHERE id=?", [id]);
+    if (insert[0].affectedRows === 0) {
+        return false
+    }
+    return true
+}
+
+export const getScheduleForTrainerModel = async () => {
+    const [rows, fields] = await pool.query("SELECT bs.date, bs.FN_or_AN session, trainee.first_name firstName,trainee.last_name lastName, vehicle.model_name modelName, vehicle.registration_number registrationNumber FROM booked_session bs,package,trainee,vehicle WHERE bs.package_id=package.id AND package.trainee_id=trainee.id AND bs.vehicle_id=vehicle.id;");
+    if (rows[0]) {
+        return rows
+    }
+    return {}
+}
+
+export const getScheduleForTraineeModel = async () => {
+    const [rows, fields] = await pool.query("SELECT bs.date, bs.FN_or_AN session, trainee.id traineeId, vehicle.model_name modelName FROM booked_session bs,package,trainee,vehicle WHERE bs.package_id=package.id AND package.trainee_id=trainee.id AND bs.vehicle_id=vehicle.id");
+    if (rows[0]) {
+        return rows
+    }
+    return {}
 }
